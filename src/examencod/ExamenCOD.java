8f6ccb9db3bdb2eb5d5957f3b7cf2a16f7c254d6
aurceramartins <aurceramartins@localhost>
2014-12-04 10:15:54 +0100

    commit cambio

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -15,7 +15,7 @@ else
           	 
 for (int x=2;x<1000;x++) {
 if (metodoMio(x))
-System.out.print(x + " ");                         	 
+System.out.print(x + " comentario");                         	 
 }         	 
 	}
 public static boolean metodoMio(int variable){
